97ffe5db01c7cc1043d7c31f8de6bac5d6b8d335
pointinternet <pointinternet@gmail.com>
2026-04-03 19:07:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ ais.on("open", () => {
 
   ais.send(JSON.stringify({
     APIKey: process.env.AIS_KEY,
-    BoundingBoxes: [[[-90, -180], [90, 180]]],
+    BoundingBoxes: [[[0, 60], [40, 100]]],
     FilterMessageTypes: ["PositionReport"]
   }));
 });
